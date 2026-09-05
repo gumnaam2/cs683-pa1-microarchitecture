7ff4c3d5b686d9cc9976afa20cbe229f1c4efa0c
gumnaam2 <siddhantkaul2006@gmail.com>
2026-09-06 03:09:56 +0530

    tiling should not have reordering. performance lost

--- task1/makerun.sh
+++ task1/makerun.sh
@@ -1,16 +1,12 @@
 make
-CORENO=21
+CORENO=0
 echo "Running on core $CORENO"
+sudo sysctl kernel.perf_event_paranoid=-1
 
 #reproducible result everytime - turbo will mess with it depending on what else the system is running
 echo 1 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo > /dev/null
-echo performance | sudo tee /sys/devices/system/cpu/cpu$CORENO/cpufreq/scaling_governor > /dev/null
-echo 100 | sudo tee /sys/devices/system/cpu/intel_pstate/min_perf_pct > /dev/null
 
 taskset -c $CORENO perf stat -e cache-misses,L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-dcache-store-misses,L1-icache-loads,L1-icache-load-misses,instructions ./bin/conv
 
-#reset settings to max performance for my laptop
+#reset turbo settings for performance for my laptop
 echo 0 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo > /dev/null
-echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor > /dev/null
-echo 100 | sudo tee /sys/devices/system/cpu/intel_pstate/min_perf_pct > /dev/null 
-echo 100 | sudo tee /sys/devices/system/cpu/intel_pstate/max_perf_pct > /dev/null
--- task1/src/conv_tile.cpp
+++ task1/src/conv_tile.cpp
@@ -1,7 +1,7 @@
 // conv_tile.cpp  STAGE 3: CACHE TILING
 
 #include "convolution.h"
-
+#include <algorithm>
 //There is notable variation between P-cores and E-cores for this task
 //In E-cores, the performance is improved by ~15%. In P-cores, it is worsened by ~20% for the same tile size
 //It is likely due to branching overhead and (from data) increased L1-icache misses - not immediately clear why
@@ -12,29 +12,46 @@ void conv_tile(const float* in, float* out, const float* ker,
     const int in_stride = W + 2 * p;  // padded row stride
     const int Tx = 64; //Tile size - x dirn
     const int Ty = 64; //Tile size - y dirn
-    float kW;
-
-    // ky, kx, oy, ox
-    for (int oy = 0; oy < H; ++oy) {
-        for (int ox = 0; ox < W; ++ox) {
-            out[oy * W + ox] = 0;
-        }
-    }
-
+    float acc;
+    int ox_end, oy_end;
+    
     for (int ty = 0; ty < H; ty += Ty){
         for (int tx = 0; tx < W; tx += Tx){
-            for (int ky = 0; ky < K; ++ky) {
-                for (int kx = 0; kx < K; ++kx) {
-                    kW = ker[ky * K + kx];
-                    for (int oy = ty; oy < ty+Ty; ++oy) {
-                        for (int ox = tx; ox < tx + Tx; ++ox) {
-                            out[oy * W + ox] += in[(oy + ky) * in_stride + (ox + kx)] * kW;;
+            oy_end = std::min(ty + Ty, H);
+            ox_end = std::min(tx + Tx, W);
+            //store to ofmap[ty -> ty + Ty - 1][tx -> tx + Tx - 1]
+            //loads ifmap[ty -> ty + Ty + ky - 1][tx -> tx + Tx + kx - 1]
+            //loads entire kernel
+            //working set: Ty*Tx + (Ty + k - 1)*(Tx + k - 1) + k^2
+            for (int oy = ty; oy < oy_end; ++oy) {
+                for (int ox = tx; ox < ox_end; ++ox) {
+                    acc = 0.0f;
+                    //load ifmap[oy -> (oy + ky)][ox -> ox + kx]
+                    //H,W,k = 2048, 2048, 3 : stride = 2050
+                    //load entire kernel: K^2 loads
+                    for (int ky = 0; ky < K; ++ky) {
+                        for (int kx = 0; kx < K; ++kx) {
+                            acc += in[(oy + ky) * in_stride + (ox + kx)] * ker[ky * K + kx];
                         }
                     }
+                    out[oy * W + ox] = acc;
                 }
             }
         }
     }
+
+    // float kW;
+    // for (int ky = 0; ky < K; ++ky) {
+    //     for (int kx = 0; kx < K; ++kx) {
+    //         kW = ker[ky * K + kx];
+    //         for (int oy = ty; oy < ty+Ty; ++oy) {
+    //             for (int ox = tx; ox < tx + Tx; ++ox) {
+    //                 out[oy * W + ox] += in[(oy + ky) * in_stride + (ox + kx)] * kW;;
+    //             }
+    //         }
+    //     }
+    // }
+
 
     // ky, kx, oy, ox
     // for (int oy = 0; oy < H; ++oy) {
